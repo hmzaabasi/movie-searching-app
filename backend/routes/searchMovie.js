eda9ac399e312be872595fd900e96c17a98063b4
Hamza Iftikhar Abbasi <52419472+hmzaabasi@users.noreply.github.com>
2020-02-02 10:05:03 +0500

    Frontend initial commit with minor backend Changes

--- backend/routes/searchMovie.js
+++ backend/routes/searchMovie.js
@@ -1,26 +1,46 @@
 const router = require("express").Router();
 const axios = require("axios");
+const NodeCache = require("node-cache");
+const moviesListCache = new NodeCache();
 require("dotenv").config();
 
-let Movie = require("../models/movie.model");
 const apiBaseUrl = process.env.API_BASE_URL;
 
 router.route("/search").get(async (req, res) => {
   try {
-    let abc = await Promise.all([
-      axios.get(apiBaseUrl + "&s=nuls&page=1"),
-      axios.get(apiBaseUrl + "&s=nuls&page=2")
-    ]);
-    let firstDataSet = abc[0].data.Search ? [...abc[0].data.Search] : [];
-    let secondDataSet = abc[1].data.Search ? [...abc[1].data.Search] : [];
-    //console.log(abc[0].data.Search);
-    // res.json(abc[0].data.Search.concat(abc[1].data.Search));
-    res.json([...firstDataSet, ...secondDataSet]);
+    let searchTerm = req.query.keyword;
+
+    if (searchTerm) {
+      if (moviesListCache.has(searchTerm)) {
+        console.log("From Cache");
+
+        res.json(moviesListCache.get(searchTerm));
+      } else {
+        console.log("From API");
+
+        let response = await Promise.all([
+          axios.get(apiBaseUrl + "&s=" + searchTerm + "&page=1"),
+          axios.get(apiBaseUrl + "&s=" + searchTerm + "&page=2")
+        ]);
+
+        let firstDataSet = response[0].data.Search
+          ? [...response[0].data.Search]
+          : [];
+        let secondDataSet = response[1].data.Search
+          ? [...response[1].data.Search]
+          : [];
+        // res.json(response[0].data.Search.concat(response[1].data.Search));
+        let moviesList = [...firstDataSet, ...secondDataSet];
+
+        moviesListCache.set(searchTerm, moviesList);
+        res.json(moviesList);
+      }
+    } else {
+      res.status(400).json({ err: "Invalid Request." });
+    }
   } catch (error) {
     res.status(400).json({ err: error.toString() });
   }
 });
 
 module.exports = router;
-
-const getMovies = async () => await axios.get(apiBaseUrl + "&s=abc");
